19ba7c060d822004dffb91d78e0d7eda87f8a3b4
TheTombstoneBackdoorOwner <mythtester500400@outlook.com>
2025-08-08 14:08:14 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -20,7 +20,6 @@ G2L["Frame_2"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
 G2L["Frame_2"]["Size"] = UDim2.new(0.65469, 0, 0.61995, 0);
 G2L["Frame_2"]["Position"] = UDim2.new(0.02187, 0, 0.04852, 0);
 
-
 G2L["Clear_3"] = Instance.new("TextButton", G2L["Frame_2"]);
 G2L["Clear_3"]["TextWrapped"] = true;
 G2L["Clear_3"]["TextScaled"] = true;
@@ -30,10 +29,7 @@ G2L["Clear_3"]["Text"] = [[CLEAR]];
 G2L["Clear_3"]["Name"] = [[Clear]];
 G2L["Clear_3"]["Position"] = UDim2.new(0.85442, 0, 0.36087, 0);
 
-
 G2L["LocalScript_4"] = Instance.new("LocalScript", G2L["Clear_3"]);
-
-
 
 G2L["Close_5"] = Instance.new("TextButton", G2L["Frame_2"]);
 G2L["Close_5"]["TextWrapped"] = true;
@@ -46,10 +42,7 @@ G2L["Close_5"]["Text"] = [[X]];
 G2L["Close_5"]["Name"] = [[Close]];
 G2L["Close_5"]["Position"] = UDim2.new(0.98091, 0, -0.00435, 0);
 
-
 G2L["LocalScript_6"] = Instance.new("LocalScript", G2L["Close_5"]);
-
-
 
 G2L["Execute_7"] = Instance.new("TextButton", G2L["Frame_2"]);
 G2L["Execute_7"]["TextWrapped"] = true;
@@ -60,10 +53,7 @@ G2L["Execute_7"]["Text"] = [[EXE]];
 G2L["Execute_7"]["Name"] = [[Execute]];
 G2L["Execute_7"]["Position"] = UDim2.new(0.85442, 0, 0.1087, 0);
 
-
 G2L["LocalScript_8"] = Instance.new("LocalScript", G2L["Execute_7"]);
-
-
 
 G2L["Inject_9"] = Instance.new("TextButton", G2L["Frame_2"]);
 G2L["Inject_9"]["TextWrapped"] = true;
@@ -74,10 +64,7 @@ G2L["Inject_9"]["Text"] = [[INJECT]];
 G2L["Inject_9"]["Name"] = [[Inject]];
 G2L["Inject_9"]["Position"] = UDim2.new(0.85442, 0, 0.61739, 0);
 
-
 G2L["LocalScript_a"] = Instance.new("LocalScript", G2L["Inject_9"]);
-
-
 
 G2L["TextLabel_b"] = Instance.new("TextLabel", G2L["Frame_2"]);
 G2L["TextLabel_b"]["BorderSizePixel"] = 0;
@@ -85,7 +72,6 @@ G2L["TextLabel_b"]["TextXAlignment"] = Enum.TextXAlignment.Left;
 G2L["TextLabel_b"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
 G2L["TextLabel_b"]["Size"] = UDim2.new(0.21002, 0, 0.04348, 0);
 G2L["TextLabel_b"]["Text"] = [[🐱  Project Frost (private)]];
-
 
 G2L["Injectedstatus_c"] = Instance.new("TextLabel", G2L["Frame_2"]);
 G2L["Injectedstatus_c"]["BorderSizePixel"] = 0;
@@ -97,10 +83,8 @@ G2L["Injectedstatus_c"]["Text"] = [[Not Injected!]];
 G2L["Injectedstatus_c"]["Name"] = [[Injectedstatus]];
 G2L["Injectedstatus_c"]["Position"] = UDim2.new(0.00955, 0, 0.94783, 0);
 
-
 G2L["Injected?_d"] = Instance.new("BoolValue", G2L["Injectedstatus_c"]);
 G2L["Injected?_d"]["Name"] = [[Injected?]];
-
 
 G2L["TextBox_e"] = Instance.new("TextBox", G2L["Frame_2"]);
 G2L["TextBox_e"]["CursorPosition"] = -1;
@@ -109,13 +93,11 @@ G2L["TextBox_e"]["Size"] = UDim2.new(0.81384, 0, 0.87826, 0);
 G2L["TextBox_e"]["Position"] = UDim2.new(0.00955, 0, 0.04783, 0);
 G2L["TextBox_e"]["Text"] = [[]];
 
-
 G2L["Frame_f"] = Instance.new("Frame", G2L["TextBox_e"]);
 G2L["Frame_f"]["BorderSizePixel"] = 0;
 G2L["Frame_f"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
 G2L["Frame_f"]["Size"] = UDim2.new(1.00293, 0, 0.01485, 0);
 G2L["Frame_f"]["Position"] = UDim2.new(0, 0, 0.99505, 0);
-
 
 G2L["Frame2_10"] = Instance.new("Frame", G2L["TextBox_e"]);
 G2L["Frame2_10"]["BorderSizePixel"] = 0;
@@ -123,7 +105,6 @@ G2L["Frame2_10"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
 G2L["Frame2_10"]["Size"] = UDim2.new(0.03519, 0, 1.00495, 0);
 G2L["Frame2_10"]["Position"] = UDim2.new(0.96774, 0, -0.00495, 0);
 G2L["Frame2_10"]["Name"] = [[Frame2]];
-
 
 G2L["ArrowUp_11"] = Instance.new("TextButton", G2L["Frame2_10"]);
 G2L["ArrowUp_11"]["TextWrapped"] = true;
@@ -136,7 +117,6 @@ G2L["ArrowUp_11"]["Text"] = [[^]];
 G2L["ArrowUp_11"]["Name"] = [[ArrowUp]];
 G2L["ArrowUp_11"]["Position"] = UDim2.new(0.08333, 0, 0, 0);
 
-
 G2L["ArrowDown_12"] = Instance.new("TextButton", G2L["Frame2_10"]);
 G2L["ArrowDown_12"]["TextWrapped"] = true;
 G2L["ArrowDown_12"]["BorderSizePixel"] = 0;
@@ -148,54 +128,84 @@ G2L["ArrowDown_12"]["Text"] = [[ˇ]];
 G2L["ArrowDown_12"]["Name"] = [[ArrowDown]];
 G2L["ArrowDown_12"]["Position"] = UDim2.new(-0.25, 0, 0.93103, 0);
 
-
 G2L["Frame_13"] = Instance.new("Frame", G2L["Frame2_10"]);
 G2L["Frame_13"]["BorderSizePixel"] = 0;
 G2L["Frame_13"]["BackgroundColor3"] = Color3.fromRGB(133, 133, 133);
 G2L["Frame_13"]["Size"] = UDim2.new(1, 0, 0.23645, 0);
 G2L["Frame_13"]["Position"] = UDim2.new(0, 0, 0.06404, 0);
 
-
 G2L["UIAspectRatioConstraint_14"] = Instance.new("UIAspectRatioConstraint", G2L["Frame_2"]);
 G2L["UIAspectRatioConstraint_14"]["AspectRatio"] = 1.82174;
 
-local RemoteToRunOffOf -- This isnt the remote name nga💔🥀 This is where to log the remote
+local RemoteToRunOffOf = nil
 
 local function C_4()
-	local script = G2L["LocalScript_4"];
-	local textbox = G2L["TextBox_e"]
-  local button = G2L["Clear_3"]
-
-button.MouseButton1Click:connect(function()
-      textbox.Text = ""
+    local button = G2L["Clear_3"]
+    local textbox = G2L["TextBox_e"]
+    button.MouseButton1Click:Connect(function()
+        textbox.Text = ""
     end)
-end;
-task.spawn(C_4);
+end
+task.spawn(C_4)
+
 local function C_6()
-	local script = G2L["LocalScript_6"];
-	local button = G2L["Close_5"]
-
-button.MouseButton1Click:connect(function()
-      G2L["ScreenGui_1"]:Destroy()
+    local button = G2L["Close_5"]
+    button.MouseButton1Click:Connect(function()
+        G2L["ScreenGui_1"]:Destroy()
     end)
-end;
-task.spawn(C_6);
-local function C_8()
-	local script = G2L["LocalScript_8"];
-	local button = G2L["Execute_7"]
-  local code = G2L["TextBox_e"]
+end
+task.spawn(C_6)
 
-button.MouseButton1Click:connect(function()
-      if RemoteToRunOffOf then do
-          RemoteToRunOffOf:Fireserver(code)
+local function C_8()
+    local button = G2L["Execute_7"]
+    local code = G2L["TextBox_e"]
+    button.MouseButton1Click:Connect(function()
+        if RemoteToRunOffOf then
+            if RemoteToRunOffOf:IsA("RemoteEvent") then
+                RemoteToRunOffOf:FireServer(code.Text)
+            else
+                RemoteToRunOffOf:InvokeServer(code.Text)
+            end
+        else
+            warn("No remote hooked Pls inject first.")
         end
     end)
-end;
-task.spawn(C_8);
-local function C_a()
-	local script = G2L["LocalScript_a"];
-	-- inject
-end;
-task.spawn(C_a);
+end
+task.spawn(C_8)
 
-return G2L["ScreenGui_1"], require;
+local function C_a()
+    local button = G2L["Inject_9"]
+    local statusLabel = G2L["Injectedstatus_c"]
+    local injectedFlag = G2L["Injected?_d"]
+
+    button.MouseButton1Click:Connect(function()
+        for _, v in pairs(game:GetDescendants()) do
+            if v:IsA("RemoteEvent") or v:IsA("RemoteFunction") then
+                local success, err = pcall(function()
+                    if v:IsA("RemoteEvent") then
+                        v:FireServer("print('test')")
+                    else
+                        v:InvokeServer("print('test')")
+                    end
+                end)
+                if success then
+                    RemoteToRunOffOf = v
+                    injectedFlag.Value = true
+                    statusLabel.BackgroundTransparency = 0
+                    statusLabel.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+                    statusLabel.TextColor3 = Color3.fromRGB(0, 255, 0)
+                    statusLabel.Text = "Injected!"
+                    return
+                end
+            end
+			end
+        injectedFlag.Value = false
+        statusLabel.BackgroundTransparency = 0
+        statusLabel.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+        statusLabel.TextColor3 = Color3.fromRGB(255, 0, 0)
+        statusLabel.Text = "Failed To Inject."
+    end)
+end
+task.spawn(C_a)
+
+return G2L["ScreenGui_1"], require
